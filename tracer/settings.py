d5671f42d66e448ad5e38c5b91b599c4a8b80122
Adam Abusang <abusangadam2001@gmail.com>
2023-10-04 04:56:02 -0600

    updated static base path

--- tracer/settings.py
+++ tracer/settings.py
@@ -150,7 +150,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [
-    BASE_DIR / "datashow/static",
+    BASE_DIR / "datashow/",
  ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
