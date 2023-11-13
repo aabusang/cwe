ae7110efc1d0de94f607c90edd166233f63abb25
Adam Abusang <abusangadam2001@gmail.com>
2023-11-13 01:33:58 -0700

    fix disallowed host

--- tracer/settings/dev.py
+++ tracer/settings/dev.py
@@ -5,5 +5,11 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'https://cwe-development10.up.railway.app']
-# CSRF_TRUSTED_ORIGINS = ['https://cwe-development10.up.railway.app']
+ALLOWED_HOSTS = [
+    'cwe-development10.up.railway.app',
+    'https://cwe-development10.up.railway.app',
+    'localhost',
+    '127.0.0.1',
+    '*'
+    ]
+CSRF_TRUSTED_ORIGINS = ['https://cwe-development10.up.railway.app']
