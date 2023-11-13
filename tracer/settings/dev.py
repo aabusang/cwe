0c68d4676ca91134303dfec1db10876c6c349a53
Adam Abusang <abusangadam2001@gmail.com>
2023-11-13 01:28:07 -0700

    add https:// before domain

--- tracer/settings/dev.py
+++ tracer/settings/dev.py
@@ -5,5 +5,5 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'cwe-development10.up.railway.app']
-CSRF_TRUSTED_ORIGINS = ['cwe-development10.up.railway.app']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'https://cwe-development10.up.railway.app']
+# CSRF_TRUSTED_ORIGINS = ['https://cwe-development10.up.railway.app']
